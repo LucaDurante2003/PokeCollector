a2beacd6e80f3dd0f44bb6cf89395352a2518d73
Luca Durante <153363437+LucaDurante2003@users.noreply.github.com>
2025-04-15 19:34:59 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,13 +2,7 @@
     <p>&copy;PokéCollector - Progetto del corso Tecnologie e Sistemi Web A.A. 2024/2025 - Sapienza Università di Roma.</p>
 </footer>
 
-<<<<<<< HEAD
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
 <script src="assets/js/scripts.js" defer></script>
 </body>
-=======
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
-        <script src="/assets/js/scripts.js" defer></script>
-    </body>
->>>>>>> 37b2542e77664374a7ef7e766549e6dfa4fbc143
 </html>
