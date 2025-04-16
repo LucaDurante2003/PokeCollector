9badc7d33c9c17485ef1e7d964eddf68ce836c66
Luca Durante <153363437+LucaDurante2003@users.noreply.github.com>
2025-04-16 16:51:25 +0200

    Delete includes directory

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-    <p>&copy;PokéCollector - Progetto del corso Tecnologie e Sistemi Web A.A. 2024/2025 - Sapienza Università di Roma.</p>
-</footer>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
-<script src="assets/js/scripts.js" defer></script>
-</body>
-</html>
